e1c87a24d896dc3b158dd3773f3677917d3d7d54
MAximUS <splintermax92@yandex.ru>
2022-08-21 17:00:45 +0300

    Final deploy

--- backend/api/views.py
+++ backend/api/views.py
@@ -104,13 +104,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     http_method_names = ('get', 'post', 'put', 'patch', 'delete', )
 
     def get_queryset(self):
-        queryset = Recipe.objects.select_related("author").prefetch_related(
-            "tags",
-            "ingredients",
-            "recipe",
-            "shopping_cart_recipe",
-            "favorite_recipe",
-        )
+        queryset = Recipe.objects.select_related("author")
         is_favorited = self.request.query_params.get(
             'is_favorited')
         is_in_shopping_cart = self.request.query_params.get(
